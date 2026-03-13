ca2e4eb512d91eabb7ab45772c5870f5c8be0d50
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-13 07:31:22 +0100

    feat: Upgrade folks to Horde 6 composer native and drop pear dependencies
    cli script renamed and moved to bin/
    Added autoloader hints.

---
+++ src/Folks.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2007-2026 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (GPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.
+ *
+ * @category Horde
+ * @package  Folks
+ * @author   Duck <duck@obala.net>
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+
+namespace Horde\Folks;
+
+/**
+ * Folks application API.
+ *
+ * This file provides a placeholder for the modern PSR-4 implementation
+ * of the Folks application.
+ *
+ * @category Horde
+ * @package  Folks
+ * @author   Duck <duck@obala.net>
+ * @license  http://www.horde.org/licenses/gpl GPL
+ */
+class Folks
+{
+}
